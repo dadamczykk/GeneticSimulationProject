ed08248c939e4b8644f808d5c15ab0d903b9a1d5
Dominik Adamczyk <dominik.adamczyk1000@gmail.com>
2023-01-01 02:03:08 +0100

    Done initial screen + saving config to file + basic thread logic

--- GeneticSimulationProject/src/main/java/agh/ics/gui/SimulationVisualizer.java
+++ GeneticSimulationProject/src/main/java/agh/ics/gui/SimulationVisualizer.java
@@ -1,4 +1,40 @@
 package agh.ics.gui;
 
+import agh.ics.ooproject.BehaviourType;
+import agh.ics.ooproject.MutationType;
+import agh.ics.ooproject.PlantType;
+import agh.ics.ooproject.WorldTypes;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+
 public class SimulationVisualizer {
+    private Stage stage;
+    private Scene scene;
+
+    private Label counterLabel;
+
+    public SimulationVisualizer(){
+
+        this.counterLabel = new Label("init");
+        this.scene = new Scene(counterLabel);
+        this.stage = new Stage();
+        this.stage.setScene(scene);
+        this.stage.show();
+    }
+
+    public void updateScene(int value){
+        this.counterLabel.setText(String.valueOf(value));
+    }
+
 }
--- GeneticSimulationProject/src/main/java/agh/ics/ooproject/SimulationEngine.java
+++ GeneticSimulationProject/src/main/java/agh/ics/ooproject/SimulationEngine.java
@@ -1,4 +1,49 @@
 package agh.ics.ooproject;
 
-public class SimulationEngine {
+import agh.ics.gui.SimulationVisualizer;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class SimulationEngine implements Runnable {
+    private int sleepTimer = 500;
+
+    private int simulationNumber;
+
+    private SimulationVisualizer simVis;
+
+    public SimulationEngine(SimulationVisualizer vis, int simNo, int newTime){
+        this.sleepTimer = newTime;
+        this.simVis = vis;
+        this.simulationNumber = simNo;
+    }
+
+    public void run(){
+        try {
+            this.testRun();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void testRun() throws InterruptedException {
+        System.out.println(sleepTimer);
+        int i = 1;
+        Thread.sleep(sleepTimer);
+        while(true){
+            int finalI = i;
+            Platform.runLater(new Runnable() {
+                @Override
+                public void run() {
+                    simVis.updateScene(finalI);
+                }
+            });
+
+//            infoLabel.setText(String.valueOf(i));
+//            System.out.println(i);
+            Thread.sleep(sleepTimer);
+            i++;
+        }
+    }
 }
